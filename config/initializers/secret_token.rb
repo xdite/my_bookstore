b672949d942009361f00443e959263ac1a005749
xdite <xdite@techbang.com.tw>
2011-06-05 22:58:59 +0800

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewbieBook::Application.config.secret_token = '0b6364026f4a7dea468f6144c2bd4738e4e28d7f266a46194f9a587ac2f4163cceb9706dc7b57fe44a1fc63ec668c0e9dcb3fb735c5f01e0f2c624dc5ed86202'
