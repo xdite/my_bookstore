8db234afd565322a0ae18e6f7e9faff6e37a93f4
xdite <xuite.joke@gmail.com>
2011-06-08 01:39:35 +0800

    add breadcramb

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ gem 'rails', '3.0.7'
 gem "compass", ">= 0.11.1"
 gem "handicraft_helper", :git => "git://github.com/handlino/handicraft_helper.git", :require => "handicraft/helper"
 
+gem "breadcrumbs_on_rails"
+
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,21 @@
 class PagesController < ApplicationController
   def welcome
+    add_breadcrumb "Home", root_path
+    add_breadcrumb "Rails 101", root_path
   end
   
   def about
+    add_breadcrumb "Home", root_path
+    add_breadcrumb "About", "/pages/about"
   end
   
   def author
+    add_breadcrumb "Home", root_path
+    add_breadcrumb "About", "/pages/author"
   end
   
   def contact
+    add_breadcrumb "Home", root_path
+    add_breadcrumb "About", "/pages/content"
   end
 end
