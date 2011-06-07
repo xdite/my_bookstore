18a001debaa88a81a59b1cd3f002cc4398145fa3
xdite <xuite.joke@gmail.com>
2011-06-08 02:32:50 +0800

    add facebook share

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+
+  /* facebook share buttom */
+  if ($("a[name=fb_share]").size() > 0) {
+    $.getScript("http://static.ak.fbcdn.net/connect.php/js/FB.Share");
+  }
+
+
+});
